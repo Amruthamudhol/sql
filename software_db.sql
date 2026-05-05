dca020edbc4c46a08947049bd9ca23a52551b21c
AmrutaMudhol <amrutashokmudhol24@gmail.com>
2026-05-05 22:21:57 +0530

    Added software database SQL file

---
+++ software_db.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE software_db;
+USE software_db;
+CREATE TABLE developer (developer_id INT PRIMARY KEY,developer_name VARCHAR(50),age TINYINT,role_name VARCHAR(50),salary DECIMAL(10));
+
+CREATE TABLE project (project_id INT PRIMARY KEY, project_name VARCHAR(100), technology VARCHAR(50), budget BIGINT);
+
+INSERT INTO developer VALUES (1, 'Amruta', 22, 'Java Developer', 45000.00),
+(2, 'Rahul', 25, 'Frontend Developer', 40000.00),
+(3, 'Sneha', 24, 'Backend Developer', 48000.00),
+(4, 'Kiran', 27, 'Full Stack Developer', 55000.00);
+
+INSERT INTO project VALUES (101, 'E-Commerce Website', 'Java', 500000),
+(102, 'Hospital Management', 'Python', 750000),
+(103, 'Student Portal', 'Spring Boot', 400000),
+(105, 'Banking Application', 'Microservices', 900000);
+
+select * from developer;
+
+select * from project ;
+
+
