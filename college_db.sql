c3dd5c0b2d345bd3e64b824d83e07bf2b792e8f9
AmrutaMudhol <amrutashokmudhol24@gmail.com>
2026-05-06 20:15:14 +0530

    added college database to sql file

--- college_db.sql
+++ college_db.sql
@@ -17,6 +17,11 @@ CREATE TABLE data_types_demo (
 
 desc data_types_demo;
 
-INSERT INTO data_types_demo VALUES (1, 22, 450000, 87.0, 'A', 'Amruta', 'Java Developer', true, '2026-05-05', '2026-05-05 10:30:00');
+INSERT INTO data_types_demo VALUES (1, 22, 450000, 87.0, 'A', 'Amruta', 'Java Developer', true, '2026-05-05', '2026-05-05 10:30:00'),
+(2, 25, 520000, 91.5, 'A', 'Rahul', 'Frontend Developer', true, '2026-05-06', '2026-05-06 11:15:00'),
+(3, 28, 610000, 76.8, 'B', 'Sneha', 'Database Analyst', true, '2026-05-07', '2026-05-07 09:45:00'),
+(4, 24, 430000, 68.2, 'C', 'Kiran', 'Support Engineer', false, '2026-05-08', '2026-05-08 02:20:00'),
+(5, 30, 750000, 88.9, 'A', 'Divya', 'Software Tester', true, '2026-05-09', '2026-05-09 04:10:00');
+
 select * from data_types_demo;
 
